b51c36834955e41b2cb291b0b6f4ee757cee2336
Areej-Farag <rerefarag60@gmail.com>
2024-11-27 15:49:40 +0200

    second project

---
+++ Resturant/js/script.js
@@ -0,0 +1,167 @@
+// var degree = [0,0,0,0,0,0]
+
+// for(var i =0 ; i < 6 ; i++){
+//     degree[i] = prompt();
+// }
+
+// for(var i =0 ; i < 6 ; i++){
+//    console.log( degree[i] ) 
+// }
+
+// for(var i =0 ; i < 6 ; i++){
+//     if(degree[i]>= 85 &&degree[i]<=100){
+//         console.log("Excellent")
+//     }
+//     else if(degree[i]>= 75){
+//         console.log("V.Good")
+//     }
+//     else if(degree[i]>= 65){
+//         console.log("Good")
+//     }
+//     else if(degree[i]>= 50){
+//         console.log("passed")
+//     }
+//     else{
+//         console.log("failed")
+//     }
+// }
+
+
+// var num1 = prompt("Enter number 1")
+// var num2 = prompt("Enter number 2")
+
+// var add = num1 + num2;
+// var multi = num1 * num2;
+
+
+// for(var i = add ; i<= multi ; i++){
+//     if (i % 2 ==0){
+//         console.log(i);
+//     }
+// }
+// var Salary = prompt("Enter Your salary")
+//      if(Salary>= 17000){
+//             console.log("Wealthy")
+//         }
+//         else if(Salary>= 9000 && Salary<17000){
+//             console.log("rich")
+//         }
+//         else{
+//             console.log("mashia be elsatr")
+//         }
+
+// var number = prompt("number of students")
+
+// var array = new Array ( number )
+// for (var i=1 ; i <= number; i++){
+//     var nameST = prompt (" enter name of student " + i)
+//     var degree = prompt (" enter  degree of student " + i)
+
+//     array [i-1] = { nameS : nameST ,
+//          degreeS : degree}
+          
+//     }
+
+//     for (var i=0; i < number; i++){
+//         console.log("studnt name " + array[i].nameS )
+//         console.log("studnt degree " + array[i].degreeS )
+//     }
+ 
+
+// var allproducts = document.querySelectorAll("#btn")
+
+// var total_price=0
+
+// allproducts.forEach(function(item){
+//     item.onclick= function(){
+//        total_price += +(item.getAttribute(price))
+        
+        
+//     }
+// })
+
+// console.log(total_price)
+// var x = prompt("Enter the number you want its factorialz")
+// var answer=1
+// if(x==0 || x== 1){
+//     console.log("the answer is")
+//     console.log ( 1 )
+// }
+// else{
+// for (var i =1 ; i<= x; i++){
+//     answer*= i
+// }
+// console.log("the answer is")
+// console.log( answer)
+// }
+// var x = prompt("Enter the number you want to check")
+// for(var i = 2 ; i< x ; i++){
+// if (x / i == 0){
+//     console.log(" the number isnt primary")
+// }
+// else{
+//     console.log(" the number is primary")
+// }
+// }
+
+// for (var i =1 ; i<=100 ; i++){
+//     if(i%5==0 && i%3==0 ){
+//         console.log("FizzBuzz")
+//     }
+//     else if(i%5==0 ){
+//         console.log("Buzz")
+//     }
+    
+//     else if(i%3==0){
+//         console.log("Fizz")
+//     }
+//     else {
+//         console.log(i)
+//     }
+// }
+
+// var arr= [1, 3, 5, 7, 9]
+// var x = prompt(" enter the number you are looking for")
+// function search(){
+//     var i =0
+//  while( arr [i]!= x && i < 5){
+//     i++
+//  }
+//  if (i==5){
+//  return -1
+//  }
+//  else{
+//     return i
+//  }
+
+// }
+
+// console.log(search())
+
+
+
+// var x = prompt(" enter the number")
+// var count = 0
+
+// for (var i =2 ;i < x ; i++){
+//          j=2
+//         while (j < x && i % j != 0){
+//                 j++
+//             }
+//         if (j == x){
+
+//             count++
+   
+//  }
+
+// }
+
+// Write the <h2> element directly to the document with styling
+// document.write('<h2 style="color: blue; font-size: 80px; text-align: center; font-family: Arial;">Elzero</h2>');
+// console.log("%cElzero %cWeb %cschool" , "color:red; font-size=40px" , "color:green; font-weight:bold; font-size=40px","background-color:blue; font-size=40px")
+// console.table(["Ahmed", "Saad", "taher" ,"Murad"]);
+
+
+
+
+
